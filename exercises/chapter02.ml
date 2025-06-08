b52867941b12184b4fa0f94d3629cd8e594eabd7
0x471 <0x471@protonmail.com>
2025-06-08 23:56:44 +0100

    organize exercise-2 file

--- exercises/chapter02.ml
+++ exercises/chapter02.ml
@@ -1,11 +1,11 @@
-(* 
-Exercise: values [★]
-What is the type and value of each of the following OCaml expressions? 
-- 7 * (1 + 2 + 3)
-- "CS " ^ string_of_int 3110
-*)
-
 let () =
+  (* 
+  Exercise: values [★]
+  What is the type and value of each of the following OCaml expressions? 
+  - 7 * (1 + 2 + 3)
+  - "CS " ^ string_of_int 3110
+  *)
+
   (* Answer: Integer, 42 *)
   let result_1: int = 7 * (1 + 2 + 3) in
   Printf.printf "result_1 = %d\n" result_1;
