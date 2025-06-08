850ad105b49a2e3ce4cccbf1a859496276deea11
0x471 <0x471@protonmail.com>
2025-06-08 23:38:12 +0100

    answer q2 in chapter-2 exercises

--- exercises/chapter02.ml
+++ exercises/chapter02.ml
@@ -13,3 +13,21 @@ Printf.printf "result_1 = %d\n" result_1;
 let result_2: string =  "CS " ^ string_of_int 3110 in
 Printf.printf "result_2 = %s\n" result_2;
 
+(*
+Exercise: operators [★★]
+- Write an expression that multiplies 42 by 10.
+- Write an expression that divides 3.14 by 2.0.
+- Write an expression that computes 4.2 raised to the seventh power.
+*)
+
+(* Answer: 420 *)
+let result_3 = 42 * 10 in
+Printf.printf "result_3 = %d\n" result_3;
+
+(* Answer: 1.570000 *)
+let result_4: float = 3.14 /. 2.0 in 
+Printf.printf "result_4 = %f\n" result_4;
+
+(* Answer: 20150.253411 *) 
+let result_5: float = 4.12 ** 7.0 in
+Printf.printf "result_5 = %f\n" result_5; 
